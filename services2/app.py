bd4619e610b4ce072bb4493372e701840d84dd6f
Rakibul <mrakibul70@gmail.com>
2024-01-04 13:17:55 +0600

    load balancer.

---
+++ services2/app.py
@@ -0,0 +1,27 @@
+import time
+
+import redis
+import requests
+from flask import Flask
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+
+@app.route('/')
+def hello():
+    get_sum = requests.post('http://web3:5000/get-sum', data={"a": 1, "b": 2})
+    total = get_sum.text
+    return f"Total sum of the numbers: {total}. from server 2\n"
